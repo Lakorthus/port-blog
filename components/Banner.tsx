f867c739470ec125da835813a79fbad948abd1cf
Julio Velezmoro <julio28ve@gmail.com>
2023-02-09 23:55:23 +0000

    responsive

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,19 +1,22 @@
-
-
 const Banner = () => {
   return (
-    <div className="flex flex-col lg:flex-row lg:space-x-5">
-        <div>
-            <h1 className="text-7xl">Lakortus Blog</h1>
-            <h2 className="mt-5 md:mt-0">
-                Welcome to{" "}
-                <span className="text-blue-500">Every Developers</span>
-            </h2>
-            Favourite Blog
-        </div>
-        <p className="mt-5 md:mt-2 max-w-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et aliquam voluptate cum facere perspiciatis unde saepe, sed exercitationem quibusdam quaerat inventore quae quidem non reprehenderit, eos nihil? Dolores, perferendis adipisci?</p>
-    </div>
-  )
-}
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+      <div>
+        <h1 className="text-7xl">Lakorthus Daily Blog</h1>
+        <h2 className="mt-5 md:mt-0">
+          Welcome to{" "}
+          <span className="underline decoration-4 decoration-orange-500">
+            Every Developers
+          </span>{" "}
+          favourite blog in the world!
+        </h2>
+      </div>
 
-export default Banner
+      <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+        New product features | The latest in coding | The weekly digest & more!
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
